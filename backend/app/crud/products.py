8c4a5bc0c8e0fefdbe6230aa03985d557c2b1b77
karukinkun <karukinkun@gmail.com>
2026-04-19 20:33:46 +0900

    ruffの設定

--- backend/app/crud/products.py
+++ backend/app/crud/products.py
@@ -5,7 +5,7 @@ def get_products(db: Session, word: str, skip: int = 0, limit: int = 6):
     query = db.query(Product)
 
     if word:
-        query = query.filter(Product.title.contains(word))
+    query = query.filter(Product.title.contains(word))
 
     total = query.count()
 
